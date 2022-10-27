cb94fa46242c6a4dd70904f837fdece151c8b489
Vadim <Vadim@NIXGAME>
2022-10-27 15:53:30 +0300

    Добавьте файлы проекта.

---
+++ Api/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using Api.Models;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Services
+{
+    public class UserService
+    {
+        private readonly IMapper _mapper;
+        private readonly DAL.DataContext _context;
+
+        public UserService(IMapper mapper, DataContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task CreateUser(CreateUserModel model)
+        {
+            var dbUser = _mapper.Map<DAL.Entites.User>(model);
+            await _context.Users.AddAsync(dbUser);
+            await _context.SaveChangesAsync();
+        }
+
+        [HttpGet]
+        public async Task<List<UserModel>> GetUsers()
+        {
+            return await _context.Users.AsNoTracking().ProjectTo<UserModel>(_mapper.ConfigurationProvider).ToListAsync();
+        }
+    }
+}
